044d4f06390a66d9e8283f5ace64c874737cc7a7
simonloh7482 <simonloh7482@gmail.com>
2021-10-07 14:41:48 +0800

    Completed API

--- api.py
+++ api.py
@@ -4,11 +4,16 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Main(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return 'Use /add/a/b to call the API which returns the sum of a and b.'
 
-api.add_resource(HelloWorld, '/')
+class Add(Resource):
+    def get(self, num1, num2):
+        return {'result': num1+num2}
+
+api.add_resource(Main, '/')
+api.add_resource(Add, '/add/<int:num1>/<int:num2>')
 
 if __name__ == '__main__':
     app.run(debug=True)
